515bd140e51244ed29233a94f94f88dead10685c
sayom88 <sayom88@yahoo.com>
2016-09-21 14:55:44 +0300

    21 Sept 2:56 PM : First code checkin of Hybrid Framework

---
+++ HybridFramework/src/main/java/config/ActionKeywords.java
@@ -0,0 +1,198 @@
+package config;
+
+import java.util.Enumeration;
+import java.util.concurrent.TimeUnit;
+
+import static executionEngine.DriverScript.OR;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import executionEngine.DriverScript;
+import utility.Log;
+
+public class ActionKeywords {
+	
+		public static WebDriver driver;
+			
+
+	public static void openBrowser(String object,String data){	 	
+		Log.info("Opening Browser");
+		try{				
+			if(data.equals("Firefox")){
+				System.setProperty("webdriver.gecko.driver", "C:\\ECLIPSE_WORKSPACE\\geckodriver-v0.10.0-win64\\geckodriver.exe");
+				driver=new FirefoxDriver();
+				   //Maximize the browser
+				  driver.manage().window().maximize();
+				Log.info("Mozilla browser started");				
+				}
+			else if(data.equals("IE")){
+				//Dummy Code, Implement you own code
+				System.setProperty("webdriver.ie.driver","C://ECLIPSE_WORKSPACE//IEDriverServer.exe");
+				driver=new InternetExplorerDriver();
+				Log.info("IE browser started");
+				}
+			else if(data.equals("Chrome")){
+				//Dummy Code, Implement you own code
+				System.setProperty("webdriver.chrome.driver", "C:/ECLIPSE_WORKSPACE/chromedriver.exe");
+				//driver = new ChromeDriver();
+				driver=new ChromeDriver();
+				 //Maximize the browser
+				driver.manage().window().maximize();
+				Log.info("Chrome browser started");
+				}
+			
+			   System.out.println("WebDriver is:"+driver);
+			  //  driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+			int implicitWaitTime=(80);
+			driver.manage().timeouts().implicitlyWait(implicitWaitTime, TimeUnit.SECONDS);
+		}catch (Exception e){
+			Log.info("Not able to open the Browser --- " + e.getMessage());
+			DriverScript.bResult = false;
+		}
+	}
+	
+	public static void navigate(String object, String data){
+		try{
+			Log.info("Navigating to URL");
+			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+			driver.get(data);
+			//driver.get(Constants.URL);
+			Thread.sleep(1000);
+			// driver.navigate().to(Constants.URL);
+		}catch(Exception e){
+			Log.info("Not able to navigate --- " + e.getMessage());
+			DriverScript.bResult = false;
+			}
+		}
+	
+	public static void click(String object1, String data){
+		try{
+			
+			Thread.sleep(2000);
+			
+			String test=object1.toString();
+			
+			Log.info("Clicking on Webelement "+ test);
+			
+			System.out.println("Clicking on Webelement:"+ test);
+			
+			System.out.println("...................Click Event Started........");
+			
+			String value="";
+			Enumeration e = OR.keys();
+
+			System.out.println("Enumeration:"+e);
+			
+			while(e.hasMoreElements())
+			{
+				//System.out.println(e.nextElement());
+			String param = (String) e.nextElement();
+			
+			System.out.println(param);
+			
+			if (param.equals(test.toString()))
+			{
+				System.out.println("Into If");
+				value=(String) OR.get(param.toString());
+				//System.out.println(value);
+				break;
+		
+			}
+			//break;
+			}
+			System.out.println(value);
+			
+	
+			
+			System.out.println("...............Click Event Completed........");
+			
+			driver.findElement(By.xpath(value)).click();
+			
+			Thread.sleep(2000);
+			
+		 }catch(Exception e){
+ 			Log.error("Not able to click --- " + e.getMessage());
+ 			DriverScript.bResult = false;
+         	}
+		}
+	
+	public static void input(String object, String data){ 
+		try{
+			
+			String test=object;
+			
+			Log.info("Entering the text in " + object);
+			
+	System.out.println("...................Input Event Started........");
+			
+	
+	String value="";
+	Enumeration e = OR.keys();
+
+	System.out.println("Enumeration:"+e);
+	
+	while(e.hasMoreElements())
+	{
+		//System.out.println(e.nextElement());
+	String param = (String) e.nextElement();
+	
+	System.out.println(param);
+	
+	if (param.equals(test.toString()))
+	{
+		System.out.println("Into If");
+		value=(String) OR.get(param.toString());
+		//System.out.println(value);
+		break;
+
+	}
+	//break;
+	}
+	System.out.println(value);
+			//System.out.println(OR.getProperty(object));
+			
+			System.out.println("...............Input Event Completed........");
+			
+			//driver.findElement(By.xpath(OR.getProperty(object))).sendKeys(data);
+			driver.findElement(By.xpath(value)).sendKeys(data);
+			Thread.sleep(2000);
+			
+		 }catch(Exception e){
+			 Log.error("Not able to Enter UserName --- " + e.getMessage());
+			 DriverScript.bResult = false;
+		 	}
+		}
+	
+
+	public static void waitFor(String object, String data) throws Exception{
+		try{
+			Log.info("Wait for 5 seconds");
+			Thread.sleep(5000);
+		 }catch(Exception e){
+			 Log.error("Not able to Wait --- " + e.getMessage());
+			 DriverScript.bResult = false;
+         	}
+		}
+
+	public static void closeBrowser(String object, String data){
+		try{
+			
+			Thread.sleep(5000);
+			
+			Log.info("Closing the browser");
+			//driver.close();
+		   driver.quit();
+		   
+		   Thread.sleep(8000);
+		   
+		 }catch(Exception e){
+			 Log.error("Not able to Close the Browser --- " + e.getMessage());
+			 DriverScript.bResult = false;
+         	}
+		}
+
+	}
---
+++ HybridFramework/src/main/java/executionEngine/DriverScript.java
@@ -0,0 +1,129 @@
+package executionEngine;
+
+import static executionEngine.DriverScript.OR;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.Test;
+
+import config.ActionKeywords;
+import config.Constants;
+import utility.ExcelUtils;
+import utility.Log;
+ 
+public class DriverScript {
+	
+	public static Properties OR;
+	public static ActionKeywords actionKeywords;
+	public static String sActionKeyword;
+	public static String sPageObject;
+	public static Method method[];
+		
+	public static int iTestStep;
+	public static int iTestLastStep;
+	public static String sTestCaseID;
+	public static String sRunMode;
+	public static String sData;
+	public static boolean bResult;
+	
+	
+	public DriverScript() throws NoSuchMethodException, SecurityException{
+		actionKeywords = new ActionKeywords();
+		method = actionKeywords.getClass().getMethods();	
+		
+	}
+	
+	@Test(priority=1)
+    public void main1() throws Exception {
+		System.out.println(Constants.Path_TestData);
+    	ExcelUtils.setExcelFile(Constants.Path_TestData);
+    	DOMConfigurator.configure("log4j.xml");
+    	String Path_OR = Constants.Path_OR;
+		FileInputStream fs = new FileInputStream(Path_OR);
+		OR= new Properties(System.getProperties());
+		OR.load(fs);
+		
+	/*	String value="";
+		Enumeration e = OR.keys();
+
+		System.out.println("Enumeration:"+e);
+		
+		while(e.hasMoreElements())
+		{
+			//System.out.println(e.nextElement());
+		String param = (String) e.nextElement();
+		
+		//System.out.println(OR.get(param));
+		
+		if (param.equals("txtbx_Appln-Yahoo_Password"))
+		{
+			value=(String) OR.get(param);
+			//System.out.println(value);
+			break;
+	
+		}
+		//break;
+		}
+		System.out.println(value);
+		*/
+		DriverScript startEngine = new DriverScript();
+	startEngine.execute_TestCase();
+		
+    }
+		
+    private void execute_TestCase() throws Exception {
+	    	int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_TestCases);
+			for(int iTestcase=1;iTestcase<iTotalTestCases;iTestcase++){
+				bResult = true;
+				sTestCaseID = ExcelUtils.getCellData(iTestcase, Constants.Col_TestCaseID, Constants.Sheet_TestCases); 
+				sRunMode = ExcelUtils.getCellData(iTestcase, Constants.Col_RunMode,Constants.Sheet_TestCases);
+				if (sRunMode.equals("Yes")){
+					Log.startTestCase(sTestCaseID);
+					iTestStep = ExcelUtils.getRowContains(sTestCaseID, Constants.Col_TestCaseID, Constants.Sheet_TestSteps);
+					iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_TestSteps, sTestCaseID, iTestStep);
+					bResult=true;
+					for (;iTestStep<iTestLastStep;iTestStep++){
+			    		sActionKeyword = ExcelUtils.getCellData(iTestStep, Constants.Col_ActionKeyword,Constants.Sheet_TestSteps);
+			    		System.out.println("sActionKeyword:"+sActionKeyword);
+			    		sPageObject = ExcelUtils.getCellData(iTestStep, Constants.Col_PageObject, Constants.Sheet_TestSteps);
+			    		System.out.println("sPageObject:"+sPageObject);
+			    		sData = ExcelUtils.getCellData(iTestStep, Constants.Col_DataSet, Constants.Sheet_TestSteps);
+			    		System.out.println("sData:"+sData);
+			    		execute_Actions();
+						if(bResult==false){
+							ExcelUtils.setCellData(Constants.KEYWORD_FAIL,iTestcase,Constants.Col_Result,Constants.Sheet_TestCases);
+							Log.endTestCase(sTestCaseID);
+							break;
+							}						
+						}
+					if(bResult==true){
+					ExcelUtils.setCellData(Constants.KEYWORD_PASS,iTestcase,Constants.Col_Result,Constants.Sheet_TestCases);
+					Log.endTestCase(sTestCaseID);	
+						}					
+					}
+				}
+    		}	
+     
+     private static void execute_Actions() throws Exception {
+	
+		for(int i=0;i<method.length;i++){
+			
+			if(method[i].getName().equals(sActionKeyword)){
+				method[i].invoke(actionKeywords,sPageObject, sData);
+				if(bResult==true){
+					ExcelUtils.setCellData(Constants.KEYWORD_PASS, iTestStep, Constants.Col_TestStepResult, Constants.Sheet_TestSteps);
+					break;
+				}else{
+					ExcelUtils.setCellData(Constants.KEYWORD_FAIL, iTestStep, Constants.Col_TestStepResult, Constants.Sheet_TestSteps);
+					ActionKeywords.closeBrowser("","");
+					break;
+					}
+				}
+			}
+     }
+     
+}
